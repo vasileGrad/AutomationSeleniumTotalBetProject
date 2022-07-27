109586d1e9318fa7849fc278f272a45a84dee4c5
vasileGrad <vasile.grad2@gmail.com>
2022-07-27 18:46:31 +0300

    Add BaseTest class

---
+++ src/main/java/training/BaseTest.java
@@ -0,0 +1,26 @@
+package training;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Endava\\Internship\\Courses\\Technologies\\Selenium\\ChromeDriver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.get("https://totalbet.ro/");
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
