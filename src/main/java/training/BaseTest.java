c1e55bd4f5c44c6748ef1bc3318d1bec390a02d8
vasileGrad <vasile.grad2@gmail.com>
2022-07-28 08:45:05 +0300

    Add SportMenuItemTest class

--- src/main/java/training/BaseTest.java
+++ src/main/java/training/BaseTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
 
     public WebDriver driver;
@@ -17,6 +19,7 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.get("https://totalbet.ro/");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @After
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,3 @@
-import com.beust.ah.A;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
